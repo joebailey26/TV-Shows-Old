f37ba8cf565189ee142c8060dc4b307dc77792e9
Joe <joe@joebailey.xyz>
2020-05-09 14:18:24 +0100

    Initial Commit

---
+++ functions/add-show.js
@@ -0,0 +1,32 @@
+import faunadb from 'faunadb'
+require('dotenv').config()
+
+/* configure faunaDB Client with our secret */
+const q = faunadb.query
+const client = new faunadb.Client({
+  secret: process.env.FAUNA_KEY
+})
+
+/* export our lambda function as named "handler" export */
+exports.handler = (event, context, callback) => {
+  /* parse the string body into a useable JS object */
+  const data = JSON.parse(event.body)
+  const item = {
+    data: data
+  }
+  /* construct the fauna query */
+  return client.query(q.Create(q.Ref("classes/tv-shows"), item))
+  .then((response) => {
+    /* Success! return the response with statusCode 200 */
+    return callback(null, {
+      statusCode: 200,
+      body: JSON.stringify("Added successfully")
+    })
+  }).catch((error) => {
+    /* Error! return the error with statusCode 400 */
+    return callback(null, {
+      statusCode: 400,
+      body: JSON.stringify(error)
+    })
+  })
+}
---
+++ functions/remove-show.js
@@ -0,0 +1,24 @@
+import faunadb from 'faunadb'
+import getId from './utils/getId'
+require('dotenv').config()
+
+const q = faunadb.query
+const client = new faunadb.Client({
+  secret: process.env.FAUNA_KEY
+})
+
+exports.handler = (event, context, callback) => {
+  const id = getId(event.path)
+  return client.query(q.Delete(q.Ref(`classes/tv-shows/${id}`)))
+  .then((response) => {
+    return callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(response)
+    })
+  }).catch((error) => {
+    return callback(null, {
+      statusCode: 400,
+      body: JSON.stringify(error)
+    })
+  })
+}
